20a67671b81d8f34982f98765bf381b4af7ef042
Praveen C <cpraveen@gmail.com>
2016-01-12 16:11:08 +0530

    Add computation of convergence rate

--- fd1conv1.py
+++ fd1conv1.py
@@ -36,6 +36,15 @@ for n in npt:
     errf.append(np.abs(dfe - dff).max())
     errc.append(np.abs(dfe - dfc).max())
     
+# Compute convergence rates
+print "%5s %17s %10s %10s" % ('h','Backward','Forward','Central')
+for i in range(1,len(hh)):
+    pb = np.log(errb[i-1]/errb[i])/np.log(2)
+    pf = np.log(errf[i-1]/errf[i])/np.log(2)
+    pc = np.log(errc[i-1]/errc[i])/np.log(2)
+    print "%e   %f   %f   %f" % (hh[i],pb,pf,pc)
+
+# Plot error convergence
 plt.loglog(hh,errb,'o-',hh,errf,'*-',hh,errc,'s--')
 plt.xlabel('h')
 plt.ylabel('Maximum error')
