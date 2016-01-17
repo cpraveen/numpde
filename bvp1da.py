c55abb555e9d0a4a6f6dcf96c404c022c8e1e8f8
Praveen C <cpraveen@gmail.com>
2016-01-17 17:47:39 +0530

    Use tdma for solving matrix

--- bvp1da.py
+++ bvp1da.py
@@ -3,6 +3,7 @@ Solve -u'' = f using finite difference
 """
 from numpy import pi, sin, linspace, zeros, ones, abs
 import matplotlib.pyplot as plt
+from tdma import *
 
 # RHS function
 def f(x):
@@ -12,28 +13,6 @@ def f(x):
 def uexact(x):
     return sin(x)
 
-# Solve using Thomas Tridiagonal algorithm
-# A x = f
-def solve(f):
-    n = len(f)
-    # construct LU decomposition
-    a = 2.0*ones(n)
-    b =-1.0*ones(n)
-    c =-1.0*ones(n)
-    for i in range(1,n):
-        b[i] = b[i]/a[i-1]
-        a[i] = a[i] - b[i]*c[i-1]
-    # solution begins
-    x = zeros(n)
-    # solve L y = f
-    x[0] = f[0]
-    for i in range(1,n):
-        x[i] = f[i] - b[i]*x[i-1]
-    # solve U x = y
-    x[-1] = x[-1]/a[-1]
-    for i in range(n-2,-1,-1):
-        x[i] = (x[i] - c[i]*x[i+1])/a[i]
-    return x
 
 xmin, xmax = 0.0, 2.0*pi
 n = 20
@@ -48,7 +27,7 @@ u[-1] = uexact(x[-1])
 b = h**2 * f(x[1:-1])
 b[0]  += u[0]
 b[-1] += u[-1]
-u[1:-1] = solve(b)
+u[1:-1] = tdma(2*ones(n-2),-ones(n-2),-ones(n-2),b)
 print "Max error = ", abs(uexact(x)-u).max()
 
 xe = linspace(xmin, xmax, 100); ue = uexact(xe)
--- bvp1dconv.py
+++ bvp1dconv.py
@@ -3,6 +3,7 @@ Solve -u'' = f using finite difference
 """
 from numpy import pi, sin, linspace, zeros, ones, abs
 import matplotlib.pyplot as plt
+from tdma import *
 
 # RHS function
 def f(x):
@@ -11,29 +12,6 @@ def f(x):
 # exact solution
 def uexact(x):
     return sin(x)
-
-# Solve using Thomas Tridiagonal algorithm
-# A x = f
-def solve(f):
-    n = len(f)
-    # construct LU decomposition
-    a = 2.0*ones(n)
-    b =-1.0*ones(n)
-    c =-1.0*ones(n)
-    for i in range(1,n):
-        b[i] = b[i]/a[i-1]
-        a[i] = a[i] - b[i]*c[i-1]
-    # solution begins
-    x = zeros(n)
-    # solve L y = f
-    x[0] = f[0]
-    for i in range(1,n):
-        x[i] = f[i] - b[i]*x[i-1]
-    # solve U x = y
-    x[-1] = x[-1]/a[-1]
-    for i in range(n-2,-1,-1):
-        x[i] = (x[i] - c[i]*x[i+1])/a[i]
-    return x
 
 # Solve the problem and compute error norm
 def error(n):
@@ -49,13 +27,14 @@ def error(n):
     b = h**2 * f(x[1:-1])
     b[0]  += u[0]
     b[-1] += u[-1]
-    u[1:-1] = solve(b)
+    u[1:-1] = tdma(2*ones(n-2),-ones(n-2),-ones(n-2),b)
     return h, abs(uexact(x)-u).max()
 
 
 h = []; err = []
-for n in [20,40,80,160]:
+for n in [20,40,80,160,320]:
     h1, err1 = error(n)
+    print "h = %e   err = %e" % (h1, err1)
     h.append(h1); err.append(err1)
 
 plt.loglog(h, err, 'o-')
