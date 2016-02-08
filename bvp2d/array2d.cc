580f160aa66025ad0853bf00cedf04fa981bec24
Praveen C <cpraveen@gmail.com>
2016-02-08 09:41:52 +0530

    Added total size variable n and some simplifications

--- bvp2d/array2d.cc
+++ bvp2d/array2d.cc
@@ -2,17 +2,29 @@
 
 using namespace std;
 
+Array2D::Array2D ()
+:
+nx (0),
+ny (0),
+n  (0)
+{
+}
+
 Array2D::Array2D (const unsigned int nx, const unsigned int ny)
 :
 nx (nx),
-ny (ny)
+ny (ny),
+n  (nx*ny),
+u  (nx*ny)
 {
-   u.resize (nx*ny);
 }
 
-void Array2D::resize(const unsigned int nx, const unsigned int ny)
+void Array2D::resize(const unsigned int nx1, const unsigned int ny1)
 {
-   u.resize (nx*ny);
+   nx = nx1;
+   ny = ny1;
+   n  = nx1 * ny1;
+   u.resize (n);
 }
 
 unsigned int Array2D::sizex() const
@@ -37,14 +49,14 @@ double& Array2D::operator() (const unsigned int i, const unsigned int j)
 
 Array2D& Array2D::operator= (const double scalar)
 {
-   for (unsigned int i=0; i<nx*ny; ++i)
+   for (unsigned int i=0; i<n; ++i)
       u[i] = scalar;
    return *this;
 }
 
 Array2D& Array2D::operator= (const Array2D& a)
 {
-   for (unsigned int i=0; i<nx*ny; ++i)
+   for (unsigned int i=0; i<n; ++i)
       u[i] = a.u[i];
    return *this;
 }
--- bvp2d/array2d.h
+++ bvp2d/array2d.h
@@ -10,7 +10,7 @@ class Array2D
 public:
    Array2D();
    Array2D(const unsigned int nx, const unsigned int ny);
-   void resize (const unsigned nx, const unsigned int ny);
+   void resize (const unsigned int nx, const unsigned int ny);
    unsigned int sizex() const;
    unsigned int sizey() const;
    double  operator()(const unsigned int i, const unsigned int j) const;
@@ -19,7 +19,7 @@ public:
    Array2D& operator= (const Array2D& u);
    
 private:
-   unsigned int nx, ny;
+   unsigned int nx, ny, n;
    vector<double> u;
 };
 
