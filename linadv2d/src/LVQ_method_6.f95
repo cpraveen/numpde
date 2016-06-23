6dfd5d68db062842fca97972ca94fa902235bd51
ashishbhole <ashishbhole07@gmail.com>
2016-06-23 19:38:38 +0530

    Update LVQ_method_6.f95

--- linadv2d/src/LVQ_method_6.f95
+++ linadv2d/src/LVQ_method_6.f95
@@ -3,13 +3,14 @@
 
       implicit none
       integer:: i, j, ii, jj
-      real   :: R, S, R2
+      real   :: R, S, R2, lambda, lambdasq
       real, intent (inout)  :: Fx(-ng+1:nx+ng, -ng+1:ny+ng),Gy(-ng+1:nx+ng, -ng+1:ny+ng)
       real, intent (in)  :: co(-ng+1:nx+ng, -ng+1:ny+ng)
       real :: u, v, xf, yf, speed(2)
       
       call LVQ_method_5(co,Fx,Gy)
 
+      lambda = dt/dx; lambdasq = lambda*lambda
       do i=1,nx+1
         do j=1,ny
            xf = xmin + (i-1)*dx
@@ -24,16 +25,16 @@
               ii = i-1 
            endif
            R2 = co(ii+1,j) - 2.0*co(ii,j) + co(ii-1,j)
-           S = (1.0/6.0)*u*(1.0-dt*dt*u*u/(dx*dx))*R2
-           Gy(i-1,j+1) = Gy(i-1,j+1) - (dt/dx)*v*S
+           S = (1.0/6.0)*u*(1.0-u*u*lambdasq)*R2
+           Gy(i-1,j+1) = Gy(i-1,j+1) - lambda*v*S
            if(v .lt. 0.0)then 
               jj = j
            else
               jj = j-1 
            endif
            R2 = co(i,jj+1) - 2.0*co(i,jj) + co(i,jj-1)
-           S = (1.0/6.0)*u*(1.0-dt*dt*u*u/(dx*dx))*R2
-           Gy(i,j+1) = Gy(i,j+1) + (dt/dx)*v*S
+           S = (1.0/6.0)*u*(1.0-lambdasq*u*u)*R2
+           Gy(i,j+1) = Gy(i,j+1) + lambda*v*S
         enddo
       enddo
 
@@ -51,16 +52,16 @@
               jj = j-1 
            endif
            R2 = co(i,jj+1) - 2.0*co(i,jj) + co(i,jj-1)
-           S = (1.0/6.0)*v*(1.0-dt*dt*v*v/(dy*dy))*R2
-           Fx(i+1,j-1) = Fx(i+1,j-1) - (dt/dy)*u*S
+           S = (1.0/6.0)*v*(1.0-lambdasq*v*v)*R2
+           Fx(i+1,j-1) = Fx(i+1,j-1) - lambda*u*S
            if(u .lt. 0.0)then 
               ii = i
            else
               ii = i-1 
            endif
            R2 = co(ii+1,j) - 2.0*co(ii,j) + co(ii-1,j)
-           S = (1.0/6.0)*v*(1.0-dt*dt*v*v/(dy*dy))*R2
-           Fx(i+1,j) = Fx(i+1,j) + (dt/dy)*u*S
+           S = (1.0/6.0)*v*(1.0-lambdasq*v*v)*R2
+           Fx(i+1,j) = Fx(i+1,j) + lambda*u*S
         enddo
       enddo
 
