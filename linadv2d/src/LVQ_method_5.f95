91d0ccde0d5bd911c3fd417f4376f0fc44178bc8
ashishbhole <ashishbhole07@gmail.com>
2016-06-23 19:38:04 +0530

    Update LVQ_method_5.f95

--- linadv2d/src/LVQ_method_5.f95
+++ linadv2d/src/LVQ_method_5.f95
@@ -3,13 +3,14 @@
 
       implicit none
       integer:: i, j, ii, jj
-      real   :: S, R2
+      real   :: S, R2, lambdasq
       real, intent (inout)  :: Fx(-ng+1:nx+ng, -ng+1:ny+ng),Gy(-ng+1:nx+ng, -ng+1:ny+ng)
       real, intent (in)  :: co(-ng+1:nx+ng, -ng+1:ny+ng)
       real :: u, v, xf, yf, speed(2) 
 
       call LVQ_method_4(co,Fx,Gy)
 
+      lambdasq = (dt*dt)/(dx*dx)
       do i=1,nx+1
         do j=1,ny
            xf = xmin + (i-1)*dx
@@ -24,7 +25,7 @@
              endif
 
              R2 = co(ii+1,j) - 2.0*co(ii,j) + co(ii-1,j)
-             S = (1.0/6.0)*u*(1.0-(dt*dt*u*u)/(dx*dx))*R2
+             S = (1.0/6.0)*u*(1.0-lambdasq*u*u)*R2
              Fx(i,j) = Fx(i,j) - S
         enddo
       enddo
@@ -44,7 +45,7 @@
               jj = j-1
            endif
            R2 = co(i,jj+1) - 2.0*co(i,jj) + co(i,jj-1)
-           S = (1.0/6.0)*v*(1.0-(dt*dt*v*v)/(dy*dy))*R2
+           S = (1.0/6.0)*v*(1.0-lambdasq*v*v)*R2
            Gy(i,j) = Gy(i,j) - S
         enddo
       enddo
