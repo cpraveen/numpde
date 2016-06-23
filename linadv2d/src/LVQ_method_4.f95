4e35659f3bee1fe3ca9976bb9bec9f4a273903d7
ashishbhole <ashishbhole07@gmail.com>
2016-06-23 19:37:17 +0530

    Update LVQ_method_4.f95

--- linadv2d/src/LVQ_method_4.f95
+++ linadv2d/src/LVQ_method_4.f95
@@ -3,13 +3,14 @@
 
       implicit none
       integer:: i, j, ii, jj
-      real   :: R, S
+      real   :: R, S, lambda
       real, intent (inout)  :: Fx(-ng+1:nx+ng, -ng+1:ny+ng),Gy(-ng+1:nx+ng, -ng+1:ny+ng)
       real, intent (in)  :: co(-ng+1:nx+ng, -ng+1:ny+ng)
       real :: u, v, xf, yf, speed(2) 
 
      call LVQ_method_3(co,Fx,Gy)
-
+ 
+      lambda = dt/dx
       do i=1,nx+1
         do j=1,ny
            xf = xmin + (i-1)*dx
@@ -24,9 +25,9 @@
               jj = j
            endif
            R = co(i,j)-co(i-1,j)
-           S = 0.5*abs(u)*(1.0-(dt/dx)*abs(u))*R
-           Gy(i,jj) = Gy(i,jj) + (dt/dx)*v*S
-           Gy(i-1,jj) = Gy(i-1,jj) - (dt/dx)*v*S
+           S = 0.5*abs(u)*(1.0-lambda*abs(u))*R
+           Gy(i,jj) = Gy(i,jj) + lambda*v*S
+           Gy(i-1,jj) = Gy(i-1,jj) - lambda*v*S
         enddo
       enddo
 
@@ -45,9 +46,9 @@
               ii = i
            endif
            R = co(i,j+1)-co(i,j)
-           S = 0.5*abs(v)*(1.0-(dt/dx)*abs(v))*R
-           Fx(ii,j) = Fx(ii,j) + (dt/dx)*u*S
-           Fx(ii,j-1) = Fx(ii,j-1) - (dt/dx)*u*S
+           S = 0.5*abs(v)*(1.0-lambda*abs(v))*R
+           Fx(ii,j) = Fx(ii,j) + lambda*u*S
+           Fx(ii,j-1) = Fx(ii,j-1) - lambda*u*S
         enddo
       enddo
 
