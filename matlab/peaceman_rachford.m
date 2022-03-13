6b24dcb632a8b49ba079bfcbfffc81ba98c68eb8
Praveen C <cpraveen@gmail.com>
2022-03-13 10:43:31 +0530

    Improve peaceman rachford

--- matlab/peaceman_rachford.m
+++ matlab/peaceman_rachford.m
@@ -4,23 +4,23 @@
 %   x0(t) = 1/2 + 1/4*cos(2*pi*t)
 %   y0(t) = 1/2 + 1/4*sin(2*pi*t)
 % Input parameters:
-%   Mx = No. of grid points along x
-%   My = No. of grid points along y
-%    T = Final time
-function error = peaceman_rachford(Mx, My, T)
+%   T   = Final time
+%   Mx  = No. of grid points along x
+%   My  = No. of grid points along y
+%   dt  = time step
+%   lplot = false or true, if not specified, it is assumed true
+function error = peaceman_rachford(T, Mx, My, dt, lplot)
+
+if nargin == 4
+   lplot = true % plot solution by default
+end
 
 mu = 1;
 dx = 1/(Mx-1);
 dy = 1/(My-1);
 
-dt = 0.01;
-
-rx = mu*dt/dx^2; ry = mu*dt/dy^2;
+[rx, ry, Ax, Ay] = get_matrices(mu, Mx, My, dx, dy, dt);
 fprintf(1,'rx,ry = %e, %e\n', rx, ry);
-ex = ones(Mx-2,1);
-Ax = spdiags([-0.5*rx*ex, (1+rx)*ex, -0.5*rx*ex], -1:1, Mx-2, Mx-2);
-ey = ones(My-2,1);
-Ay = spdiags([-0.5*ry*ey, (1+ry)*ey, -0.5*ry*ey], -1:1, My-2, My-2);
 
 x  = linspace(0,1,Mx);
 y  = linspace(0,1,My);
@@ -29,18 +29,20 @@ y  = linspace(0,1,My);
 u    = zeros(Mx,My);
 unph = zeros(Mx,My);
 
-u = ExactSolution(X,Y,0);
-
-contour(X,Y,u), axis square, pause(2)
+u = ExactSolution(X,Y,0); % Initial condition
+if lplot == true, contour(X,Y,u), axis square, pause(2), end;
 
 t = 0;
 while t < T
-   if t+dt > T, dt = T - t; end
+   if t+dt > T
+      dt = T - t;
+      [rx, ry, Ax, Ay] = get_matrices(mu, Mx, My, dx, dy, dt);
+   end
    % Set boundary conditions
-   unph(1:Mx,1   ) = ExactSolution(X(1:Mx,1   ),Y(1:Mx,1   ),t+0.5*dt);
-   unph(1:Mx,My  ) = ExactSolution(X(1:Mx,My  ),Y(1:Mx,My  ),t+0.5*dt);
-   unph(1,   1:My) = ExactSolution(X(1,   1:My),Y(1,   1:My),t+0.5*dt);
-   unph(Mx,  1:My) = ExactSolution(X(Mx,  1:My),Y(Mx,  1:My),t+0.5*dt);
+   unph(1:Mx,1   ) = ExactSolution(X(1:Mx,1   ),Y(1:Mx,1   ),t+0.5*dt); % bottom
+   unph(1:Mx,My  ) = ExactSolution(X(1:Mx,My  ),Y(1:Mx,My  ),t+0.5*dt); % top
+   unph(1,   1:My) = ExactSolution(X(1,   1:My),Y(1,   1:My),t+0.5*dt); % left
+   unph(Mx,  1:My) = ExactSolution(X(Mx,  1:My),Y(Mx,  1:My),t+0.5*dt); % right
    for k=2:My-1
       rhs = 0.5*dt*SourceTerm(X(2:Mx-1,k), Y(2:Mx-1,k),t+0.5*dt);
       rhs = rhs + u(2:Mx-1,k) + ...
@@ -50,10 +52,10 @@ while t < T
       unph(2:Mx-1,k) = Ax \ rhs;
    end
    % Set boundary conditions
-   u(1:Mx,1   ) = ExactSolution(X(1:Mx,1   ),Y(1:Mx,1   ),t+dt);
-   u(1:Mx,My  ) = ExactSolution(X(1:Mx,My  ),Y(1:Mx,My  ),t+dt);
-   u(1,   1:My) = ExactSolution(X(1,   1:My),Y(1,   1:My),t+dt);
-   u(Mx,  1:My) = ExactSolution(X(Mx,  1:My),Y(Mx,  1:My),t+dt);
+   u(1:Mx,1   ) = ExactSolution(X(1:Mx,1   ),Y(1:Mx,1   ),t+dt); % bottom
+   u(1:Mx,My  ) = ExactSolution(X(1:Mx,My  ),Y(1:Mx,My  ),t+dt); % top
+   u(1,   1:My) = ExactSolution(X(1,   1:My),Y(1,   1:My),t+dt); % left
+   u(Mx,  1:My) = ExactSolution(X(Mx,  1:My),Y(Mx,  1:My),t+dt); % right
    for j=2:Mx-1
       rhs = 0.5*dt*SourceTerm(X(j,2:My-1), Y(j,2:My-1),t+0.5*dt);
       rhs = rhs + unph(j,2:My-1) + ...
@@ -64,14 +66,28 @@ while t < T
    end
    umin=min(min(u)); umax=max(max(u));
    t = t + dt; fprintf(1,'t=%e, min,max=%e, %e\n',t,umin,umax);
-   st=strcat('t = ', num2str(t));
-   contour(X,Y,u), axis square, title(st), pause(.1)
+   if lplot == true
+      st=strcat('t = ', num2str(t));
+      contour(X,Y,u), axis square, title(st), pause(.1)
+   end
 end
 
 % Compute L2 norm of error
 ue = ExactSolution(X,Y,T);
 error = norm(u-ue,'fro');
 error = error*sqrt(dx*dy);
+
+end
+%-------------------------------------------------------------------------------
+% Construct 1d matrices
+%-------------------------------------------------------------------------------
+function [rx, ry, Ax,Ay] = get_matrices(mu, Mx, My, dx, dy, dt)
+
+rx = mu*dt/dx^2; ry = mu*dt/dy^2;
+ex = ones(Mx-2,1);
+Ax = spdiags([-0.5*rx*ex, (1+rx)*ex, -0.5*rx*ex], -1:1, Mx-2, Mx-2);
+ey = ones(My-2,1);
+Ay = spdiags([-0.5*ry*ey, (1+ry)*ey, -0.5*ry*ey], -1:1, My-2, My-2);
 
 end
 %-------------------------------------------------------------------------------
